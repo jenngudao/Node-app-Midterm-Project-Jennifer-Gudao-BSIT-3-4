96123eb41e199472062aea1f91f2e44407686531
Jennifer Gudao <jenng2713@gmail.com>
2022-10-20 10:41:08 +0800

    update add.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const write = require('./write');
-const cmd = process.argv[2];
-write(cmd);
+const add = require('./add')
+const read = require('./read')
+const write = require('./write')
+
+//Get user input
+const data = process.argv
+
+var note = {}
+if (data[2] == 'add') {
+    //Build object
+    note = {
+        id: data[3],
+        title: data[4],
+        body: data[5]
+    }
+    //Get old note value
+    var oldNote = read()
+    //add note to note.txt
+    add(note, oldNote)
+}
+if (data[2] == 'read') {
+    //impport present
+    const present = require('./present')
+    present(read())
+}
